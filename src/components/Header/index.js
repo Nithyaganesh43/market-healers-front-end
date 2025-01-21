854ee43c07dd724d900d4198e1749d45a21b059e
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-01-21 22:07:13 +0530

    error cleared

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -12,7 +12,6 @@ const Headers = styled.header`
   background-color: rgb(32, 32, 32);
   color: var(--white);
   z-index: 180;
-  width:100vw;
   position: relative;
   @media only Screen and (max-width: 64em) {
     padding: 0.5rem 5rem;
@@ -60,7 +59,7 @@ const Logo = styled.a`
 `;
 
 const Nav = styled.nav`
-  width: auto;
+  width: 25rem;
   background-color: rgb(32, 32, 32);
   max-width: 40rem;
   display: flex;
@@ -142,14 +141,14 @@ const MobileMenu = styled.nav`
   position: fixed;
   top: 10%;
   right: 10%;
-  font-size:18px;
+  font-size: 18px;
   border-radius: 20px;
   z-index: ${(props) => (props.clicked ? '1000' : '-1')};
   background-color: rgba(53, 53, 63, 0.95);
   transition: opacity 0.5s ease-in-out, visibility 0.5s ease-in-out;
   a {
     font-weight: 600;
-    font-size:1.3rem;
+    font-size: 1.3rem;
     line-height: 1.5;
     color: var(--white);
     text-decoration: none;
