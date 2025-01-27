412015fb1be548117840ca37d1f764280bd76226
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-01-27 20:54:09 +0530

    changed seache

--- src/HomeSections/Testimonials/index.js
+++ src/HomeSections/Testimonials/index.js
@@ -1,11 +1,10 @@
-import React, { lazy } from "react";
-import styled from "styled-components";
+import React, { lazy } from 'react';
+import styled from 'styled-components';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-import Slider from "react-slick";
-import "../../../node_modules/slick-carousel/slick/slick.css";
-import "../../../node_modules/slick-carousel/slick/slick-theme.css";
-
-const Card = lazy(() => import("../../components/Card/index"));
+const Card = lazy(() => import('../../components/Card/index'));
 
 const Section = styled.div`
   display: flex;
@@ -18,96 +17,90 @@ const Section = styled.div`
 
 const Title = styled.h1`
   color: #0a0b10;
-  display: inline-block;
   font-size: calc(1rem + 1.5vw);
   margin-top: 1.5rem;
   position: relative;
+
   &::before {
-    content: "";
+    content: '';
     height: 1px;
     width: 50%;
     position: absolute;
     left: 50%;
     bottom: 0;
     transform: translate(-50%, 0.5rem);
-    /* or 100px */
     border-bottom: 2px solid var(--purple);
   }
 `;
 
-const Carousal = styled.div`
+const CarouselWrapper = styled.div`
   width: 50vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  @media only Screen and (max-width: 40em) {
+  @media (max-width: 768px) {
     width: 90vw;
-    .slick-slider .slick-arrow {
-      display: none;
-    }
   }
+
   .slick-slider .slick-arrow:before {
     color: #0a0b10;
     font-size: 1.5rem;
-    @media only Screen and (max-width: 40em) {
-      display: none;
-    }
   }
+
   .slick-slider .slick-dots button:before {
     color: #0a0b10;
-    font-size: 1.5rem;
   }
-  .slick-slide.slick-active {
-    display: flex;
+
+  .slick-slide {
+    display: flex !important;
     justify-content: center;
     align-items: center;
-    flex-direction: column;
-    margin: 0;
-    padding: 0;
-    margin-bottom: 3rem;
   }
 `;
 
 const Testimonials = () => {
-   const settings = {
-     dots: true,
-     infinite: true,
-     speed: 1000,
-     slidesToShow: 1,
-     slidesToScroll: 1,
-     autoplay: true,
-     autoplaySpeed: 5000,  
-   };
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  };
+
   return (
     <Section>
       <Title>Words that inspire and guide.</Title>
-      <Carousal>
+      <CarouselWrapper>
         <Slider {...settings}>
           <Card
             text="If you don't find a way to make money while you sleep, you will work until you die."
-            name="Warren Buffett(Sage of Omaha)"
+            name="Warren Buffett (Sage of Omaha)"
             image="avatar-1"
           />
-
           <Card
             text="When something is important enough, you do it even if the odds are not in your favor."
-            name="Elon Musk(SpaceX)"
+            name="Elon Musk (SpaceX)"
             image="avatar-2"
           />
-
           <Card
-            text="Success is a new chapter after failure"
-            name="Harshad mehta (Big Bull)"
+            text="Success is a new chapter after failure."
+            name="Harshad Mehta (Big Bull)"
             image="avatar-3"
           />
-
           <Card
             text="Compound interest is the eighth wonder of the world. He who understands it, earns it … he who doesn't … pays it."
             name="Albert Einstein (Genius)"
             image="avatar-4"
           />
         </Slider>
-      </Carousal>
+      </CarouselWrapper>
     </Section>
   );
 };
